0cd69035c4bfb2b3835a887cdbaeaf483768b27d
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-05 18:15:07 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -3,6 +3,6 @@ echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 count=0
 while :; do
     count=$((count+1))
-    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$(hostname)$(printf "%04d" $count) -r community-pools.mysrv.cloud:10300 -p rpc -m 8;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$(hostname)$(printf "%04d" $count) -r community-pools.mysrv.cloud:10300 -p rpc -m 4;
     sleep 5;
 done
