41d9dd4c1da66ae699de9d2d9a8bb87d6fa49f8f
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-13 09:56:18 +0700

    Update rpc_mining_test.sh
    Thêm tên máy

--- rpc_mining_test.sh
+++ rpc_mining_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
+    rand_name=Z-Flod2
     rand_num=$((RANDOM % 9999 + 1))
-    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.SS-Z-Fold2 -r community-pools.mysrv.cloud:10300 -p rpc -m 4;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$rand_name -r community-pools.mysrv.cloud:10300 -p rpc -m 2;
     sleep 5;
 done
