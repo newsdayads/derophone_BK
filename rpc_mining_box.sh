eea4a2c75ffe4c6a20d8a5a53b287366463203f2
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-14 21:39:02 +0700

    Update rpc_mining_box.sh

--- rpc_mining_box.sh
+++ rpc_mining_box.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-	rand-name=BS7
     rand_num=$((RANDOM % 9999 + 1))
-	
-    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -p rpc -m 8;
-
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.BS7-$rand_num -r community-pools.mysrv.cloud:10300 -p rpc -m 8;
     sleep 5;
 done
