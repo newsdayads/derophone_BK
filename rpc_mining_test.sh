756349addfad49ceb4724c58c51328fc4635992c
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-17 14:54:08 +0700

    Update rpc_mining_test.sh

--- rpc_mining_test.sh
+++ rpc_mining_test.sh
@@ -7,6 +7,6 @@ echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
     rand_name=Z-Fold-2
     rand_num=$((RANDOM % 9999 + 1))
-    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r community-pools.mysrv.cloud:10300 -opmem -disable-api -m 2;
+    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r community-pools.mysrv.cloud:10300 -m 2;
     sleep 5;
 done
