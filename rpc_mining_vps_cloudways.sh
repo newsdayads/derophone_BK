f5e27b5e567fc8ad6b31bdf1730ccf3b5d851df1
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-29 15:41:06 +0700

    Update rpc_mining_vps_cloudways.sh

--- rpc_mining_vps_cloudways.sh
+++ rpc_mining_vps_cloudways.sh
@@ -4,6 +4,6 @@ while :; do
     rand_name=cloudways
     rand_num=$((RANDOM % 9999 + 1))
     "Ví CLI"
-        ./astrominerv19 -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -r2 nodent2.cpumining.cloud:10100 -m 21;
+        ./astrominerv19 -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r1 community-pools.mysrv.cloud:10300 -r dero.rabidmining.com:10300 -r2 nodent2.cpumining.cloud:10100 -m 21;
     sleep 5;
 done
