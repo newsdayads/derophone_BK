dc4a904b0db9c36463bdbdb16bbbc668f9dee646
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-29 22:06:52 +0700

    Update rpc_mining_note9.sh

--- rpc_mining_note9.sh
+++ rpc_mining_note9.sh
@@ -7,6 +7,6 @@ echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
     rand_name=SS-Note-9
     rand_num=$((RANDOM % 9999 + 1))
-    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -r2 nodent2.cpumining.cloud:10100 -m;
+    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -r2 nodent2.cpumining.cloud:10100 -m 8;
     sleep 5;
 done
