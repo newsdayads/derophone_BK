3fc36c8a5c7bc38410f818e56e4037ad6355e548
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-03-31 16:37:00 +0700

    Update rpc_mining_vps.sh

--- rpc_mining_vps.sh
+++ rpc_mining_vps.sh
@@ -4,6 +4,6 @@ while :; do
     rand_name=VPS-Azure
     rand_num=$((RANDOM % 9999 + 1))
     "Ví CLI"
-    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.VPS-$rand_num -r dero.rabidmining.com:10300 -m 21;
+    ./astrominerv19 -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.VPS-$rand_num -r dero.rabidmining.com:10300 -m 21;
     sleep 5;
 done
