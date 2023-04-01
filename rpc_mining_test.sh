c811de93ae0b335411b37ebcd10e0cc9d1d589dd
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-01 11:05:08 +0700

    Update rpc_mining_test.sh

--- rpc_mining_test.sh
+++ rpc_mining_test.sh
@@ -7,6 +7,6 @@ echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
     rand_name=Z-Fold2
     rand_num=$((RANDOM % 9999 + 1))
-    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r dero.rabidmining.com:10300 -m 7;
+    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r dero.rabidmining.com:10300 -m 8;
     sleep 5;
 done
