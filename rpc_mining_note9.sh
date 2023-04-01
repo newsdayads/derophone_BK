a8625ab39e1d003edf2bf0ceba848bdffab4c29f
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-01 08:20:47 +0700

    Update rpc_mining_note9.sh

--- rpc_mining_note9.sh
+++ rpc_mining_note9.sh
@@ -5,7 +5,7 @@
 #Đang chạy ví CLI
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    rand_name=SS-Note-9
+    rand_name=SS-Note9
     rand_num=$((RANDOM % 9999 + 1))
     ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r dero.rabidmining.com:10300 -m 8;
     sleep 5;
