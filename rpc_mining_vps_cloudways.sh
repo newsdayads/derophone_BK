e138df6b9d841a8c8eac25484b36416d79cafeb6
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-10 08:48:53 +0700

    Update rpc_mining_vps_cloudways.sh

--- rpc_mining_vps_cloudways.sh
+++ rpc_mining_vps_cloudways.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    rand_name=VPS-cloudways
+    rand_name=VPS-Cloudways
     rand_num=$((RANDOM % 9999 + 1))
     "Ví CLI"
-        ./astrominerv19 -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r dero.rabidmining.com:10300 -m 21;
+    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num  -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -m 8;
     sleep 5;
 done
