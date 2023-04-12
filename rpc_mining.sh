1dc6cf9522a1e9b7032466e232e27e5633245884
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-13 03:24:27 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
+    rand_name=S7
     rand_num=$((RANDOM % 9999 + 1))
     "Ví CLI"
     ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num  -r2 dero-node-orionure-sg.mysrv.cloud:10300 -r1 community-pools.mysrv.cloud:10300 -r dero.rabidmining.com:10300 -m 8;
